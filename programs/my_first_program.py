bb26d4e8546f34b0c2655b881c2ef72fbe37f596
xNORAGAMIx <noragami0021@gmail.com>
2024-07-01 14:19:58 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,19 +1,26 @@
 from nada_dsl import *
 
 def nada_main():
-    # Define a party
+
     party1 = Party(name="Party1")
+    
+    my_int1 = SecretInteger(Input(name="my_int1", party=party1)) #original ley
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1)) #test key
 
-    # Define secret integer inputs for the party
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    modulus = Integer(100000) #used for enscryption
 
-    # Perform computations on the inputs
-    sum_result = my_int1 + my_int2  # Compute the sum of the two integers
-    product_result = my_int1 * my_int2  # Compute the product of the two integers
+    encrptedKey = Integer(67890) #using a key to encrypt the original key
 
-    # Return the outputs of the computations
+    encrypt = (my_int1 + encrptedKey) % modulus #creating encrypt data using modulus
+
+    decrypt = (encrypt - my_int2 + modulus) % modulus #creating decrypt data using the encrypt data
+    
+    #checking if the original key matches the decrypted key
+    condition =  decrypt == my_int1 
+    result = condition.if_else(Integer(1), Integer(0))
+
+    #returns 1 if the key matches, else returns 0
     return [
-        Output(sum_result, "sum_output", party1),  # Output the sum result
-        Output(product_result, "product_output", party1)  # Output the product result
+        Output(result, "1 True, 0 False", party1)
+        
     ]
